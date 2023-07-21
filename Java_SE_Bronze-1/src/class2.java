6dc87980a960eb5887233f271df581a3bf22705f
valleymouth1216 <okeitan3280@gmail.com>
2023-07-22 00:00:15 +0900

    20230722

--- Java_SE_Bronze-1/src/class2.java
+++ Java_SE_Bronze-1/src/class2.java
@@ -1,9 +1,21 @@
 
 public class class2 {
+	
+	//オーバーロード
+	public static void main(String args[]){
+		  int n = plus(10, 7);
+		  System.out.println(n);
 
+		  double d = plus(3.14, 7.4);
+		  System.out.println(d);
+		}
 
-	
-	
-	
+		private static int plus(int n1, int n2){
+		  return n1 + n2;
+		}
+
+		private static double plus(double d1, double d2){
+		  return d1 + d2;
+		}
 }
 
