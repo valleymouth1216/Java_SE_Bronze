cef7b1e7e66b6515baf10cea8699dcf64fbac8f3
valleymouth1216 <okeitan3280@gmail.com>
2023-07-30 09:46:12 +0900

    20230730

--- Java_SE_Bronze-1/src/class4.java
+++ Java_SE_Bronze-1/src/class4.java
@@ -1,8 +1,19 @@
 
 public class class4 {
 
-	
-	
-	
-	
+	public class Main {
+	    
+	    public static void main(String args[]) {
+	        msg("佐藤");
+	        msg("山田", "はじめまして");
+	    }
+	    
+	    static void msg(String name) {
+	        System.out.println("こんにちは、" + name + "です");
+	    }
+	    
+	    static void msg(String name, String greeting) {
+	        System.out.println(greeting + "、" + name + "です");
+	    }
+	}
 }
