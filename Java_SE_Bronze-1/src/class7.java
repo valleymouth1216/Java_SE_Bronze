aa42d471d19a2f181f3b8b33b92555ae403c1875
valleymouth1216 <okeitan3280@gmail.com>
2023-08-03 23:42:28 +0900

    20230803

--- Java_SE_Bronze-1/src/class7.java
+++ Java_SE_Bronze-1/src/class7.java
@@ -1,4 +1,17 @@
 
 public class class7 {
 
+    public static void disp_str() {
+        System.out.println("パラメータがありません");
+}
+
+public static void disp_str(String str) {
+        System.out.println(str);
+}
+
+public static void main(String[] args) {
+        disp_str();
+        disp_str("文字列を渡しました");
+}        
+
 }
