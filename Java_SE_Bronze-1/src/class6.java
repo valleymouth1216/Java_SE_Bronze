899e7de13f5f53f4823f454c8010d4d98251ece6
valleymouth1216 <okeitan3280@gmail.com>
2023-08-04 22:56:03 +0900

    20230804

--- Java_SE_Bronze-1/src/class6.java
+++ Java_SE_Bronze-1/src/class6.java
@@ -1,5 +1,18 @@
 
 public class class6 {
 
-	
+    public static void disp_str(String str) {
+        System.out.println(str);
+}
+
+public static int disp_str(String str) {
+        System.out.println(str);
+        return 0;
+}
+
+public static void main(String[] args) {
+        int r = disp_str("文字列を渡しました"); // これは判別できるが
+        disp_str("もう一度、文字列を渡しました"); // これは判別できない。
+}        
+}
 }
