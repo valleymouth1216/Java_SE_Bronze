1fa01389863dabfa79d36698c952ff7ce53158f6
valleymouth1216 <okeitan3280@gmail.com>
2023-08-11 22:48:35 +0900

    20230811

--- Java_SE_Bronze-1/src/class9.java
+++ Java_SE_Bronze-1/src/class9.java
@@ -1,4 +1,11 @@
 
 public class class9 {
+    public static void disp_val(float val) {
+        System.out.println("float型の" + val);
+}
 
+
+public static void main(String[] args) {
+        disp_val(100);
+}    
 }
