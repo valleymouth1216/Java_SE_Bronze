2c6405a19683d198dd042c082673c9a077196f76
valleymouth1216 <okeitan3280@gmail.com>
2023-08-18 23:29:35 +0900

    20230818

---
+++ Java_SE_Bronze-1/src/class11.java
@@ -0,0 +1,18 @@
+
+public class class11 {
+	public static void main(String args[]){
+		  int n = plusInt(10, 7);
+		  System.out.println(n);
+
+		  double d = plusDouble(3.14, 7.4);
+		  System.out.println(d);
+		}
+
+		private static int plusInt(int n1, int n2){
+		  return n1 + n2;
+		}
+
+		private static double plusDouble(double d1, double d2){
+		  return d1 + d2;
+		}
+}
