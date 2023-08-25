b1ecafe5467a937f39d36b2bec4a4c1f14e92f5c
valleymouth1216 <okeitan3280@gmail.com>
2023-08-25 23:29:27 +0900

    20230825

--- Java_SE_Bronze-1/src/class14.java
+++ Java_SE_Bronze-1/src/class14.java
@@ -1,4 +1,31 @@
-
 public class class14 {
-
+ 
+	public static void main(String[] args) {
+		Person person = new Person();
+		person.setInfo("山田");
+		person.setInfo(30);
+		person.setInfo("佐藤", 24);
+	}
+}
+ 
+class Person {
+	String name;
+	int age;
+	
+	public void setInfo(String name) {
+		this.name = name;
+		System.out.println("nameを" + name + "に上書きしました。");
+	}
+	
+	public void setInfo(String name, int age) {
+		this.name = name;
+		this.age = age;
+		System.out.println("nameを" + name +  " ageを" + age +  "に上書きしました。");
+	}
+	
+	public void setInfo(int age) {
+		this.age = age;
+		System.out.println("ageを" + age + "に上書きしました。");
+	}
+	
 }
