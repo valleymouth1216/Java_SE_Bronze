9957cb1dfea54b945c30fbd71288d06bdccb8e55
valleymouth1216 <okeitan3280@gmail.com>
2023-08-31 23:55:22 +0900

    20230831

--- Java_SE_Bronze-1/src/class17.java
+++ Java_SE_Bronze-1/src/class17.java
@@ -1,4 +1,22 @@
 
 public class class17 {
 
+
+String color;
+
+public SampleRobot() {  // コンストラクタ
+    System.out.println("こんにちは。");
+}
+
+public SampleRobot(String color) {  // 色を指定する場合のコンストラクタ
+    this.color = color;
+    System.out.println("こんにちは。私の色は" + color + "です。");
+}
+}
+
+public class Java_overload {
+public static void main(String[] args) {
+    SampleRobot robot = new SampleRobot();
+    SampleRobot blueRobot = new SampleRobot("青");
+}
 }
