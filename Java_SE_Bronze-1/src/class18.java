5f0de7efb287d35e90fb61bf0d7090e36ef0e604
valleymouth1216 <okeitan3280@gmail.com>
2023-09-06 23:20:06 +0900

    20230906

--- Java_SE_Bronze-1/src/class18.java
+++ Java_SE_Bronze-1/src/class18.java
@@ -1,10 +1,4 @@
 
 public class class18 {
-    public static void main(String[] args) throws Exception{
 
-        OverLoadSample ols = new OverLoadSample();
-        ols.hoge();
-        ols.hoge("AAA");
-        ols.hoge("AAA", 123);
-        o
 }
