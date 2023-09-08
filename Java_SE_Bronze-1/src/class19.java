05266e1771c2b219ff5bb475f85e2c88f152dffc
valleymouth1216 <okeitan3280@gmail.com>
2023-09-08 19:16:11 +0900

    20230908

--- Java_SE_Bronze-1/src/class19.java
+++ Java_SE_Bronze-1/src/class19.java
@@ -1,4 +1,33 @@
 
 public class class19 {
 
+    //メソッド１
+    public void hoge(){
+        System.out.println("hoge-None");
+    }
+
+    //メソッド２
+    public void hoge(String a){
+        System.out.println("hoge-String");
+    }
+
+    //メソッド３
+    public void hoge(String a, int i){
+        System.out.println("hoge-StringInt");
+    }
+
+    //メソッド４
+    public void hoge(int i, String a){
+        System.out.println("hoge-intString");
+    }
+
+    //throws句が異なってもオーバーロードにはならない（エラーになる）
+    //public void hoge() throws Exception{
+    //    System.out.println("hoge");
+    //}
+
+    //アクセス修飾子が異なってもオーバーロードにはならない（エラーになる）
+    //private void hoge(){
+    //    System.out.println("hoge");
+    //}
 }
