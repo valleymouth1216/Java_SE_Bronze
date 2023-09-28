493540d00752655f425922e62a4e119d9ba3165b
valleymouth1216 <okeitan3280@gmail.com>
2023-09-28 23:51:01 +0900

    20230928

--- Java_SE_Bronze-1/src/class10.java
+++ Java_SE_Bronze-1/src/class10.java
@@ -36,30 +36,7 @@ public static void main(String args[]){
     System.out.println("double + double");
     return d1 + d2;
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+ 
   
   
 }
