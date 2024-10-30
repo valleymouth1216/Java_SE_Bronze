243bc8cb7ed806ee40dd1324369da074072ab96a
valleymouth1216 <okeitan3280@gmail.com>
2024-10-30 20:08:27 +0900

    20241030

--- Java_SE_Bronze-1/src/class21.java
+++ Java_SE_Bronze-1/src/class21.java
@@ -7,12 +7,7 @@ public class class21 {
 	    System.out.println(add(1, 2.3));    // (4)が呼び出される
 	    System.out.println(add(1.2, 3));    // (5)が呼び出される
 	}
-	
-	
-	
-	
-	
-	
+		
 	
 	static int add(int x, int y) { // (1) int, int
 	    int num = x + y;
